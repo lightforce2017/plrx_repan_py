5493d739577c1b41f896ed301f4459667d04eda8
LanaOST <light1force@gmail.com>
2020-08-09 19:58:38 +0300

    Count commits within function:

--- repan.py
+++ repan.py
@@ -1,6 +1,6 @@
 import requests
-import json
 import time
+import sys
 
 ##################################################################################
 #                               Validate URL
@@ -102,146 +102,96 @@ def isMonth(m):
 def isYear(y):
     if y.isdigit():
         return 2008 <= int(y) <= 2050
+
+
+##################################################################################
+#                               Count commits
+##################################################################################
         
+def countCommits(repoURL, startDate, stopDate, branch='master'):
+    # startDate - start of period
+    # stopDate - end of period
+    # branch - branch for search within ('master' bu deafault)
+    # 
+    # Starting from the first page, results per page 100
+    r = requests.get('https://api.github.com/repos/django/django/commits', params={'since': startDate, 'until': stopDate, 'branch': branch, 'page': 1, 'per_page': 100})
+    data=r.json()
+
+    # N to store the number of records in JSON
+    N = len(data)
+
+    # Future list with author names
+    names = []
+
+    # Make a list of authors
+    for i in range(N):
+        names.append(data[i]['commit']['author']['name'])
+    # Based on the list of authors names, we form a dictionary in which, opposite each author name, its number of commits
+    # The author is added to the dictionary automatically if they do not exist yet, 
+    # otherwise the number of his commits increases by 1
+    d = dict() 
+    for c in names:
+        d[c] = d.get(c,0) + 1
+
+    # m stores the number of pages as a result of the request
+    m = 2
+    # 60 is a limit on the number of requests for an unauthorized user
+    while data != [] and m < 61:
+        # reset the lists for the next check and save
+        data = []
+        names = []
         
-# TODO1: сделать в виде параметров командной строки
-repoURL = "http://github.com/django/django"       #if is public 
+        # get the next page
+        r = requests.get('https://api.github.com/repos/django/django/commits', params={'since': startDate, 'until': stopDate, 'branch': branch, 'page': m, 'per_page': 100})
+        
+        # add to the results
+        data = r.json()
+        N = len(data)
+        for i in range(N):
+            names.append(data[i]['commit']['author']['name'])
+        for c in names:
+            d[c] = d.get(c,0) + 1
+        
+        # little message for user
+        print('Pages analyzed: '+str(m),end="\r")
+        m+=1
+        # timeout so as not to exceed the number of requests per second
+        time.sleep(0.5) 
 
-# TODO3: добавить проверку формата вводимой даты с автоисправлением, если возможно
-startDate = '2014-12-22'
-stopDate = '2017-01-19'
-
-branch = 'master'
-
-
-
-
-'''def count_user_commits(user):
-    r = requests.get('https://api.github.com/users/%s/repos' % user)
-    repos = json.loads(r.content)
-
-    for repo in repos:
-        if repo['fork'] is True:
-            # skip it
-            continue
-        n = count_repo_commits(repo['url'] + '/commits')
-        repo['num_commits'] = n
-        yield repo
-
-def count_repo_commits(commits_url, _acc=0):
-    r = requests.get(commits_url)
-    commits = json.loads(r.content)
-    n = len(commits)
-    if n == 0:
-        return _acc
-    link = r.headers.get('link')
-    if link is None:
-        return _acc + n
-    next_url = find_next(r.headers['link'])
-    if next_url is None:
-        return _acc + n
-    # try to be tail recursive, even when it doesn't matter in CPython
-    return count_repo_commits(next_url, _acc + n)
+    # Sort authors by the number of their commits, in descending order:
+    # No. Author   commits_count
+    return sorted(d.items(), reverse=True,  key=lambda x: x[1])
+        
 
 
-# given a link header from github, find the link for the next url which they use for pagination
-def find_next(link):
-    for l in link.split(','):
-        a, b = l.split(';')
-        if b.strip() == 'rel="next"':
-            return a.strip()[1:-1]
-
-
+# Аргументы командной строки:
+#repoURL = "http://github.com/django/django"       #if is public 
+#startDate = '2014-12-22'
+#stopDate = '2017-01-19'
+#branch = 'master'
 if __name__ == '__main__':
-    import sys
+    repoURL = "http://github.com/django/django"       #if is public 
+    startDate = '2014-12-22'
+    stopDate = '2017-01-19'
+    branch = 'master'
     try:
         user = sys.argv[1]
     except IndexError:
         print "Usage: %s <username>" % sys.argv[0]
         sys.exit(1)
-    total = 0
-    for repo in count_user_commits(user):
-        print "Repo `%(name)s` has %(num_commits)d commits, size %(size)d." % repo
-        total += repo['num_commits']
-
-    print "Total commits: %d" % total
-
-'''
-
-
-# Создаем запрос с параметрами: 
-# startDate - дата начала периода
-# stopDate - дата конца периода
-# branch - ветка
-# 
-# Начинаем с первой страницы, количество результатов на страницу 100
-
-#########################################################
-#                  request для авторов коммитов
-#########################################################
-r = requests.get('https://api.github.com/repos/django/django/commits', params={'since': startDate, 'until': stopDate, 'branch': branch, 'page': 1, 'per_page': 100})
-
-# Парсим в JSON
-data=r.json()
-
-#str = json.loads(r.text)
-# N для хранения количества записей в JSON
-N = len(data)
-
-# Будущий список с именами авторов
-names = []
-
-# Формируем список авторов
-for i in range(N):
-    names.append(data[i]['commit']['author']['name'])
-# На основе списка авторов names формируем словарь, в котором напротив каждого имени автора его количество коммитов
-# Автор добавляется в словарь автоматически, если их еще нет, в ином случае количество его коммитов увеличивается на 1
-d = dict() 
-for c in names:
-    d[c] = d.get(c,0) + 1
-   
-f=open("scrap.json","a",encoding='utf-8')
-ss = json.dumps(data, indent=4, sort_keys=True)
-f.write('Page 1\n\n')
-f.write(ss)
-# m - количество страниц в результате запроса
-# 60 - ограничение на количество запросов для неавторизованного пользователя
-m = 2
-while data != [] and m < 61:
-    data = []
-    names = []
-    # получаем следующую страницу
-    r = requests.get('https://api.github.com/repos/django/django/commits', params={'since': startDate, 'until': stopDate, 'branch': branch, 'page': m, 'per_page': 100})
-    
-    # добавляем к нашим результатам
-    data = r.json()
-    N = len(data)
-    for i in range(N):
-        names.append(data[i]['commit']['author']['name'])
-    for c in names:
-        d[c] = d.get(c,0) + 1
-    f=open("scrap.json","a",encoding='utf-8')
-    ss = json.dumps(data, indent=4, sort_keys=True)
-    f.write('Page '+str(m)+'\n\n')
-    f.write(ss)
-    f.close()
-    print('Проанализировано страниц: '+str(m),end="\r")
-    m+=1
-    # таймаут, чтобы не превысить количество запросов в секунду
-    time.sleep(0.5) 
-m -=1
+        
+    listofTuples = countCommits(repoURL, startDate, stopDate, branch)
+    k = 0
+    for elem in listofTuples :
+        if k>=30:
+            break
+        print(f"{k+1:3}. {elem[0]:25} {elem[1]:6}")
+        k+=1
 
 
-# Сортируем авторов по количеству их коммитов, в убывающем порядке
-# Получаем список, где каждый элемент в виде:
-# номер. Имя_автора   количево_коммитов
-k = 0
-listofTuples = sorted(d.items(), reverse=True,  key=lambda x: x[1])
-for elem in listofTuples :
-    if k>=30:
-        break
-    print(f"{k+1:3}. {elem[0]:25} {elem[1]:6}")
-    k+=1
+
+
+
 
 
 
