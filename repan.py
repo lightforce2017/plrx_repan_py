7d00843520c21d6e3a993673c5c2d5bff70675cc
LanaOST <light1force@gmail.com>
2020-08-09 17:54:02 +0300

    Added validation to repoURL

--- repan.py
+++ repan.py
@@ -1,7 +1,40 @@
 import requests
 import json
-import random
 import time
+
+
+# According to the form validation messages on Create repository page,
+# Github repo's name should include alphanumeric symbols and '-', '_', '.'
+def validRepo(repo):
+    return ''.join(c for c in repo if  c not in '-_.').isalpha()
+
+# Rules got from https://github.com/shinnn/github-username-regex:
+# According to the form validation messages on Join Github page,
+# + Github username may only contain alphanumeric characters or hyphens.
+# + Github username cannot have multiple consecutive hyphens.
+# + Github username cannot begin or end with a hyphen.
+# + Maximum is 39 characters.
+def validUser(user):
+    alpha = ''.join(user.split('-')).isalpha()
+    hyphens = user.find('--')==-1
+    behyp = user.find('-')!=0 and user.find('-')!=len(user)-1
+    maxlen = len(user)<=39
+    return alpha and hyphens and behyp and maxlen
+
+def ValidRepoURL(repoURL):
+    if repoURL.find('github.com') == -1:
+        print('Wrong repo URL. The address should be like this: http://github.com/author/repo.')
+        return False
+    else:
+        if repoURL.find('http://') > -1 or repoURL.find('https://') > -1:
+            url = repoURL.split('//')[1]
+        else:
+            url = repoURL
+        url = url.split('github.com/')[1].split('/')
+        user = url[0]
+        repo = url[1]
+        return validRepo(repo) and validUser(user)
+
 
 # TODO1: сделать в виде параметров командной строки
 repoURL = "http://github.com/django/django"       #if is public 
@@ -12,20 +45,8 @@ stopDate = '2017-01-19'
 
 branch = 'master'
 
-# список браузеров для ротации в запросе для преодоления защиты от скраперов
-user_agent_list = [
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
-]
-url = 'https://httpbin.org/headers'
 
-# ============== Временный код start ==============
-# TODO1: заготовка для командной строки из скрипта, который дает результаты по собственному репозиторию автора
-# https://api.github.com/repos/django/django/commits?until=2010-10-10&since=2009-01-01&branch=master&page=25&per_page=100
-# curl -H "Accept: application/vnd.github.cloak-preview" https://api.github.com/search/commits?q=repo:django/django+author-date:<2016-01-01
+
 
 '''def count_user_commits(user):
     r = requests.get('https://api.github.com/users/%s/repos' % user)
@@ -78,13 +99,7 @@ if __name__ == '__main__':
     print "Total commits: %d" % total
 
 '''
-# ============== Временный код end  ==============
 
-# Выбираем случайный браузер из списка
-user_agent = random.choice(user_agent_list)
-
-# Устанавливаем заголовки 
-headers = {'User-Agent': user_agent, 'Accept': 'application/vnd.github.cloak-preview'}
 
 # Создаем запрос с параметрами: 
 # startDate - дата начала периода
@@ -96,9 +111,7 @@ headers = {'User-Agent': user_agent, 'Accept': 'application/vnd.github.cloak-pre
 #########################################################
 #                  request для авторов коммитов
 #########################################################
-#r = requests.get('https://api.github.com/repos/django/django/commits', params={'since': startDate, 'until': stopDate, 'branch': branch, 'page': 1, 'per_page': 100}, headers=headers)
-r = requests.get('https://api.github.com/repos/django/django/pulls', params={'state': 'closed', 'base': branch, 'page': 1, 'per_page': 100}, headers=headers)
-
+r = requests.get('https://api.github.com/repos/django/django/commits', params={'since': startDate, 'until': stopDate, 'branch': branch, 'page': 1, 'per_page': 100})
 
 # Парсим в JSON
 data=r.json()
@@ -111,34 +124,34 @@ N = len(data)
 names = []
 
 # Формируем список авторов
-#for i in range(N):
-#    names.append(data[i]['commit']['author']['name'])
+for i in range(N):
+    names.append(data[i]['commit']['author']['name'])
 # На основе списка авторов names формируем словарь, в котором напротив каждого имени автора его количество коммитов
 # Автор добавляется в словарь автоматически, если их еще нет, в ином случае количество его коммитов увеличивается на 1
-'''d = dict() 
+d = dict() 
 for c in names:
     d[c] = d.get(c,0) + 1
-'''
    
 f=open("scrap.json","a",encoding='utf-8')
 ss = json.dumps(data, indent=4, sort_keys=True)
 f.write('Page 1\n\n')
 f.write(ss)
 # m - количество страниц в результате запроса
+# 60 - ограничение на количество запросов для неавторизованного пользователя
 m = 2
 while data != [] and m < 61:
     data = []
     names = []
     # получаем следующую страницу
-    #r = requests.get('https://api.github.com/repos/django/django/commits', params={'since': startDate, 'until': stopDate, 'branch': branch, 'page': m, 'per_page': 100}, headers=headers)
-    r = requests.get('https://api.github.com/repos/django/django/pulls', params={'state': 'closed', 'base': branch, 'page': m, 'per_page': 100}, headers=headers)
+    r = requests.get('https://api.github.com/repos/django/django/commits', params={'since': startDate, 'until': stopDate, 'branch': branch, 'page': m, 'per_page': 100}, headers=headers)
+    
     # добавляем к нашим результатам
     data = r.json()
-    #N = len(data)
-    #for i in range(N):
-    #    names.append(data[i]['commit']['author']['name'])
-    #for c in names:
-    #    d[c] = d.get(c,0) + 1
+    N = len(data)
+    for i in range(N):
+        names.append(data[i]['commit']['author']['name'])
+    for c in names:
+        d[c] = d.get(c,0) + 1
     f=open("scrap.json","a",encoding='utf-8')
     ss = json.dumps(data, indent=4, sort_keys=True)
     f.write('Page '+str(m)+'\n\n')
@@ -147,11 +160,10 @@ while data != [] and m < 61:
     print('Проанализировано страниц: '+str(m),end="\r")
     m+=1
     # таймаут, чтобы не превысить количество запросов в секунду
-    
     time.sleep(0.5) 
 m -=1
 
-'''
+
 # Сортируем авторов по количеству их коммитов, в убывающем порядке
 # Получаем список, где каждый элемент в виде:
 # номер. Имя_автора   количево_коммитов
@@ -162,16 +174,6 @@ for elem in listofTuples :
         break
     print(f"{k+1:3}. {elem[0]:25} {elem[1]:6}")
     k+=1
-'''
 
 
 
-'''
-# TODO4: убрать из релиза
-# запись в файл для анализа результата запроса и формирования кода
-f=open("scrap.json","w",encoding='utf-8')
-ss = json.dumps(data, indent=4, sort_keys=True)
-#print(ss)
-f.write(ss)
-f.close()
-'''
